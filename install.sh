11e2205789045b63ea952b0c25ec420fe2b30103
chuvachellaV1 <tenebraehorribiles@gmail.com>
2026-03-27 21:12:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,11 @@
 #sudo pacman -S virtualbox-guest-utils
 #VBoxClient-all
 
-sudo pacman -S i3-wm i3status i3lock dmenu xorg-server xorg-xinit feh alacritty sddm
+sudo pacman -S i3-wm i3status i3lock dmenu xorg-server xorg-xinit feh alacritty sddm picom firefox
 sudo systemctl enable --now sddm.service
 echo "exec i3" > ~/.xinitrc
 echo 'for_window [class="^.*"] border pixel 2' >> ~/.config/i3/config
 echo 'gaps inner 10' >> ~/.config/i3/config
 echo 'gaps outer 5' >> ~/.config/i3/config
+echo 'exec_always --no-startup-id picom -f' >> ~/.config/i3/config
 startx
