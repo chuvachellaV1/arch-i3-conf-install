fe92911aa3e4acb427bfa170eec053dbaede5c51
chuvachellaV1 <tenebraehorribiles@gmail.com>
2026-03-27 17:24:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@
 #sudo pacman -S virtualbox-guest-utils
 #VBoxClient-all
 
-sudo pacman -S i3-wm i3status i3lock dmenu xorg-server xorg-xinit feh alacritty
+sudo pacman -S i3-wm i3status i3lock dmenu xorg-server xorg-xinit feh alacritty sddm
+sudo systemctl enable --now sddm.service
 echo "exec i3" > ~/.xinitrc
 echo 'for_window [class="^.*"] border pixel 2' >> ~/.config/i3/config
 echo 'gaps inner 10' >> ~/.config/i3/config
