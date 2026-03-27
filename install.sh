48a344e84338b8e2db863a550058a17fa4419774
chuvachellaV1 <tenebraehorribiles@gmail.com>
2026-03-27 17:12:35 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#systemctl enable --now NetworkManager
+
+#sudo pacman -S virtualbox-guest-utils
+#VBoxClient-all
+
+sudo pacman -S i3-wm i3status i3lock dmenu xorg-server xorg-xinit feh alacritty
+echo "exec i3" > ~/.xinitrc
+echo 'for_window [class="^.*"] border pixel 2' >> ~/.config/i3/config
+echo 'gaps inner 10' >> ~/.config/i3/config
+echo 'gaps outer 5' >> ~/.config/i3/config
+startx
