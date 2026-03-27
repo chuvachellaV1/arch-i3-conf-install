206e87cf6e7709c464ac6dc7b69d8358075f0c4e
chuvachellaV1 <tenebraehorribiles@gmail.com>
2026-03-28 00:16:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-#systemctl enable --now NetworkManager
+sudo pacman -S git i3-wm i3status i3lock dmenu xorg-server xorg-xinit feh alacritty sddm picom firefox
 
+#systemctl enable --now NetworkManager
+sudo systemctl enable --now sddm.service
 #sudo pacman -S virtualbox-guest-utils
 #VBoxClient-all
 
-sudo pacman -S i3-wm i3status i3lock dmenu xorg-server xorg-xinit feh alacritty sddm picom firefox
-sudo systemctl enable --now sddm.service
+mkdir -p ~/.config/i3
+if [ ! -f ~/.config/i3/config ]; then
+    cp /etc/i3/config ~/.config/i3/config
+fi
+
 echo "exec i3" > ~/.xinitrc
 echo 'for_window [class="^.*"] border pixel 2' >> ~/.config/i3/config
 echo 'gaps inner 10' >> ~/.config/i3/config
 echo 'gaps outer 5' >> ~/.config/i3/config
 echo 'exec_always --no-startup-id picom -f' >> ~/.config/i3/config
+mkdir ~/.config/alacritty/
+git clone https://github.com/chuvachellaV1/arch-i3-conf-install
+cd arch-i3-conf-install
+mv alacritty.toml ~/.config/alacritty/
 startx
